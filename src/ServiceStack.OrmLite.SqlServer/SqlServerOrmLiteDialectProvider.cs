eafaddc439872619b685e9f9bf009ac561b7ddb7
Travis Watt <travis.watt@gmail.com>
2012-12-25 16:46:57 +0000

    UseUnicode now adds N for escaped string
    UseUnicode divides DefaultStringLength by 2
    If the string length exceeds 4000/8000 the field length is MAX
    For complex types where the length is not defined, MAX is used instead of DefaultStringLength

--- src/ServiceStack.OrmLite.SqlServer/SqlServerOrmLiteDialectProvider.cs
+++ src/ServiceStack.OrmLite.SqlServer/SqlServerOrmLiteDialectProvider.cs
@@ -2,151 +2,269 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.IO;
+using System.Text;
+using ServiceStack.Text;
 
-namespace ServiceStack.OrmLite.SqlServer 
+namespace ServiceStack.OrmLite.SqlServer
 {
     public class SqlServerOrmLiteDialectProvider : OrmLiteDialectProviderBase<SqlServerOrmLiteDialectProvider>
-	{
-		public static SqlServerOrmLiteDialectProvider Instance = new SqlServerOrmLiteDialectProvider();
+    {
+        public static SqlServerOrmLiteDialectProvider Instance = new SqlServerOrmLiteDialectProvider();
 
-		private static DateTime timeSpanOffset = new DateTime(1900,01,01);
+        private static DateTime timeSpanOffset = new DateTime(1900, 01, 01);
 
-		public SqlServerOrmLiteDialectProvider()
-		{
-			base.AutoIncrementDefinition = "IDENTITY(1,1)";
-			base.StringColumnDefinition = "VARCHAR(8000)";
-			base.GuidColumnDefinition = "UniqueIdentifier";
-			base.RealColumnDefinition = "FLOAT";
-		    base.BoolColumnDefinition = "BIT";
-			base.DecimalColumnDefinition = "DECIMAL(38,6)";
-			base.TimeColumnDefinition = "TIME"; //SQLSERVER 2008+
-		    base.BlobColumnDefinition = "VARBINARY(MAX)";
+        public SqlServerOrmLiteDialectProvider()
+        {
+            base.AutoIncrementDefinition = "IDENTITY(1,1)";
+            base.StringColumnDefinition = "VARCHAR(8000)";
+            base.GuidColumnDefinition = "UniqueIdentifier";
+            base.RealColumnDefinition = "FLOAT";
+            base.BoolColumnDefinition = "BIT";
+            base.DecimalColumnDefinition = "DECIMAL(38,6)";
+            base.TimeColumnDefinition = "TIME"; //SQLSERVER 2008+
+            base.BlobColumnDefinition = "VARBINARY(MAX)";
 
-			base.InitColumnTypeMap();
-		}
+            base.InitColumnTypeMap();
+        }
 
-		public override IDbConnection CreateConnection(string connectionString, Dictionary<string, string> options)
-		{
-			var isFullConnectionString = connectionString.Contains(";");
+        public override IDbConnection CreateConnection(string connectionString, Dictionary<string, string> options)
+        {
+            var isFullConnectionString = connectionString.Contains(";");
 
-			if (!isFullConnectionString)
-			{
-				var filePath = connectionString;
+            if(!isFullConnectionString)
+            {
+                var filePath = connectionString;
 
-				var filePathWithExt = filePath.ToLower().EndsWith(".mdf")
-					? filePath 
-					: filePath + ".mdf";
+                var filePathWithExt = filePath.ToLower().EndsWith(".mdf")
+                    ? filePath
+                    : filePath + ".mdf";
 
-				var fileName = Path.GetFileName(filePathWithExt);
-				var dbName = fileName.Substring(0, fileName.Length - ".mdf".Length);
+                var fileName = Path.GetFileName(filePathWithExt);
+                var dbName = fileName.Substring(0, fileName.Length - ".mdf".Length);
 
-				connectionString = string.Format(
-				@"Data Source=.\SQLEXPRESS;AttachDbFilename={0};Initial Catalog={1};Integrated Security=True;User Instance=True;",
-					filePathWithExt, dbName);
-			}
+                connectionString = string.Format(
+                @"Data Source=.\SQLEXPRESS;AttachDbFilename={0};Initial Catalog={1};Integrated Security=True;User Instance=True;",
+                    filePathWithExt, dbName);
+            }
 
-			if (options != null)
-			{
-				foreach (var option in options)
-				{
-					if (option.Key.ToLower() == "read only")
-					{
-						if (option.Value.ToLower() == "true")
-						{
-							connectionString += "Mode = Read Only;";
-						}
-						continue;
-					}
-					connectionString += option.Key + "=" + option.Value + ";";
-				}
-			}
+            if(options != null)
+            {
+                foreach(var option in options)
+                {
+                    if(option.Key.ToLower() == "read only")
+                    {
+                        if(option.Value.ToLower() == "true")
+                        {
+                            connectionString += "Mode = Read Only;";
+                        }
+                        continue;
+                    }
+                    connectionString += option.Key + "=" + option.Value + ";";
+                }
+            }
 
-			return new SqlConnection(connectionString);
-		}
+            return new SqlConnection(connectionString);
+        }
 
         public override string GetQuotedTableName(ModelDefinition modelDef)
         {
-            if (!modelDef.IsInSchema)
+            if(!modelDef.IsInSchema)
                 return base.GetQuotedTableName(modelDef);
 
             var escapedSchema = modelDef.Schema.Replace(".", "\".\"");
             return string.Format("\"{0}\".\"{1}\"", escapedSchema, NamingStrategy.GetTableName(modelDef.ModelName));
         }
 
-		public override object ConvertDbValue(object value, Type type)
-		{
-			try
-			{
-				if (value == null || value is DBNull) return null;
+        public override object ConvertDbValue(object value, Type type)
+        {
+            try
+            {
+                if(value == null || value is DBNull) return null;
 
-				if (type == typeof(bool) && !(value is bool))
-				{
-					var intVal = Convert.ToInt32(value.ToString());
-					return intVal != 0;
-				}
+                if(type == typeof(bool) && !(value is bool))
+                {
+                    var intVal = Convert.ToInt32(value.ToString());
+                    return intVal != 0;
+                }
 
-				if (type == typeof(TimeSpan) && value is DateTime)
-				{
-					var dateTimeValue = (DateTime)value;
-					return dateTimeValue - timeSpanOffset;
-				}
+                if(type == typeof(TimeSpan) && value is DateTime)
+                {
+                    var dateTimeValue = (DateTime)value;
+                    return dateTimeValue - timeSpanOffset;
+                }
 
-				return base.ConvertDbValue(value, type);
-			}
-			catch (Exception ex)
-			{
-				throw;
-			}
-		}
+                return base.ConvertDbValue(value, type);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
 
-		public override string GetQuotedValue(object value, Type fieldType)
-		{
-			if (value == null) return "NULL";
+        public override string GetQuotedValue(object value, Type fieldType)
+        {
+            if(value == null) return "NULL";
 
-			if (fieldType == typeof(Guid))
-			{
-				var guidValue = (Guid)value;
-				return string.Format("CAST('{0}' AS UNIQUEIDENTIFIER)", guidValue);
-			}
-			if (fieldType == typeof(DateTime))
-			{
-				var dateValue = (DateTime)value;
-				const string iso8601Format = "yyyyMMdd HH:mm:ss.fff";
-				return base.GetQuotedValue(dateValue.ToString(iso8601Format), typeof(string));
-			}
-			if (fieldType == typeof(bool))
-			{
-				var boolValue = (bool)value;
-				return base.GetQuotedValue(boolValue ? 1 : 0, typeof(int));
-			}
+            if(fieldType == typeof(Guid))
+            {
+                var guidValue = (Guid)value;
+                return string.Format("CAST('{0}' AS UNIQUEIDENTIFIER)", guidValue);
+            }
+            if(fieldType == typeof(DateTime))
+            {
+                var dateValue = (DateTime)value;
+                const string iso8601Format = "yyyyMMdd HH:mm:ss.fff";
+                return base.GetQuotedValue(dateValue.ToString(iso8601Format), typeof(string));
+            }
+            if(fieldType == typeof(bool))
+            {
+                var boolValue = (bool)value;
+                return base.GetQuotedValue(boolValue ? 1 : 0, typeof(int));
+            }
 
-			return base.GetQuotedValue(value, fieldType);
-		}
+            if(!fieldType.UnderlyingSystemType.IsValueType && fieldType != typeof(string))
+            {
+                if(TypeSerializer.CanCreateFromString(fieldType))
+                {
+                    return "'" + EscapeParam(TypeSerializer.SerializeToString(value)) + "'";
+                }
 
-		public override long GetLastInsertId(IDbCommand dbCmd)
-		{
-			dbCmd.CommandText = "SELECT SCOPE_IDENTITY()";
-			return dbCmd.GetLongScalar();
-		}
+                throw new NotSupportedException(
+                    string.Format("Property of type: {0} is not supported", fieldType.FullName));
+            }
 
-		public override SqlExpressionVisitor<T> ExpressionVisitor<T>()
-		{
-			return new SqlServerExpressionVisitor<T>();
-		}
+            if(fieldType == typeof(float))
+                return ((float)value).ToString(CultureInfo.InvariantCulture);
 
-		public override bool DoesTableExist(IDbCommand dbCmd, string tableName)
-		{
-			var sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = {0}"
-				.SqlFormat(tableName);
+            if(fieldType == typeof(double))
+                return ((double)value).ToString(CultureInfo.InvariantCulture);
 
-			//if (!string.IsNullOrEmpty(schemaName))
-			//    sql += " AND TABLE_SCHEMA = {0}".SqlFormat(schemaName);
+            if(fieldType == typeof(decimal))
+                return ((decimal)value).ToString(CultureInfo.InvariantCulture);
 
-			dbCmd.CommandText = sql;
-			var result = dbCmd.GetLongScalar();
-			
-			return result > 0;
-		}
-	}
+            if(UseUnicode)
+            {
+                return ShouldQuoteValue(fieldType)
+                    ? "N'" + EscapeParam(value) + "'"
+                    : value.ToString();
+            }
+
+            return ShouldQuoteValue(fieldType)
+                ? "'" + EscapeParam(value) + "'"
+                : value.ToString();
+        }
+
+        public override long GetLastInsertId(IDbCommand dbCmd)
+        {
+            dbCmd.CommandText = "SELECT SCOPE_IDENTITY()";
+            return dbCmd.GetLongScalar();
+        }
+
+        public override SqlExpressionVisitor<T> ExpressionVisitor<T>()
+        {
+            return new SqlServerExpressionVisitor<T>();
+        }
+
+        public override bool DoesTableExist(IDbCommand dbCmd, string tableName)
+        {
+            var sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = {0}"
+                .SqlFormat(tableName);
+
+            //if (!string.IsNullOrEmpty(schemaName))
+            //    sql += " AND TABLE_SCHEMA = {0}".SqlFormat(schemaName);
+
+            dbCmd.CommandText = sql;
+            var result = dbCmd.GetLongScalar();
+
+            return result > 0;
+        }
+
+        private const int MaxLengthUnicodeString = 4000;
+        private const int MaxLengthNonUnicodeString = 8000;
+
+        public override string GetColumnDefinition(string fieldName, Type fieldType,
+            bool isPrimaryKey, bool autoIncrement, bool isNullable,
+            int? fieldLength, int? scale, string defaultValue)
+        {
+            string fieldDefinition;
+
+            if(fieldType == typeof(string))
+            {
+                int length;
+
+                if(fieldLength == null)
+                {
+                    length = UseUnicode ? DefaultStringLength / 2 : DefaultStringLength;
+                }
+                else
+                {
+                    length = fieldLength.Value;
+                }
+
+                var lengthAsString = length.ToString();
+
+                if((UseUnicode && length > MaxLengthUnicodeString) || length > MaxLengthNonUnicodeString)
+                {
+                    lengthAsString = "MAX";
+                }
+
+                fieldDefinition = string.Format(StringLengthColumnDefinitionFormat, lengthAsString);
+            }
+            else
+            {
+                if(!DbTypeMap.ColumnTypeMap.TryGetValue(fieldType, out fieldDefinition))
+                {
+                    fieldDefinition = this.GetUndefinedColumnDefinition(fieldType, fieldLength);
+                }
+            }
+
+            var sql = new StringBuilder();
+            sql.AppendFormat("{0} {1}", GetQuotedColumnName(fieldName), fieldDefinition);
+
+            if(isPrimaryKey)
+            {
+                sql.Append(" PRIMARY KEY");
+                if(autoIncrement)
+                {
+                    sql.Append(" ").Append(AutoIncrementDefinition);
+                }
+            }
+            else
+            {
+                if(isNullable)
+                {
+                    sql.Append(" NULL");
+                }
+                else
+                {
+                    sql.Append(" NOT NULL");
+                }
+            }
+
+            if(!string.IsNullOrEmpty(defaultValue))
+            {
+                sql.AppendFormat(DefaultValueFormat, defaultValue);
+            }
+
+            return sql.ToString();
+        }
+
+        protected override string GetUndefinedColumnDefinition(Type fieldType, int? fieldLength)
+        {
+            if(TypeSerializer.CanCreateFromString(fieldType))
+            {
+                if(fieldLength == null)
+                {
+                    return string.Format(StringLengthColumnDefinitionFormat, "MAX");
+                }
+
+                return string.Format(StringLengthColumnDefinitionFormat, fieldLength.GetValueOrDefault(DefaultStringLength));
+            }
+
+            throw new NotSupportedException(
+                string.Format("Property of type: {0} is not supported", fieldType.FullName));
+        }
+    }
 }
---
+++ src/ServiceStack.OrmLite.SqlServerTests/LongStringTests.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ServiceStack.Common.Tests.Models;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite.SqlServer;
+
+namespace ServiceStack.OrmLite.SqlServerTests
+{
+    [TestFixture]
+    public class UnicodeStringTest : OrmLiteTestBase
+    {
+        public class ModelWithIdAndLongString
+        {
+            public int Id { get; set; }
+
+            [StringLength(4001)]
+            public string LongString { get; set; }
+        }
+
+        class ComplexType
+        {
+            public int Id { get; set; }
+
+            [StringLength(250)]
+            public IList<string> StringList { get; set; }
+            [StringLength(4000)]
+
+            public Dictionary<Guid, string> Dictionary { get; set; }
+            public IList<ComplexSubType> SubTypes { get; set; }
+
+            internal class ComplexSubType
+            {
+                public int Field1 { get; set; }
+                public string Field2 { get; set; }
+                public Guid Field3 { get; set; }
+                public DateTime Field4 { get; set; }
+            }
+        }
+
+        [Test]
+        public void Complex_Types_Should_Use_Max_For_Undefined_String_Length()
+        {
+            OrmLiteConfig.DialectProvider.UseUnicode = true;
+            var createTableSql = OrmLiteConfig.DialectProvider.ToCreateTableStatement(typeof(ComplexType));
+            Console.WriteLine("createTableSql: " + createTableSql);
+            Assert.That(createTableSql.Contains(@"""StringList"" NVARCHAR(250)"), Is.True);
+            Assert.That(createTableSql.Contains(@"""Dictionary"" NVARCHAR(4000)"), Is.True);
+            Assert.That(createTableSql.Contains(@"""SubTypes"" NVARCHAR(MAX)"), Is.True);
+        }
+
+        [Test]
+        public void Unicode_table_should_have_max_string_length_of_4000()
+        {
+            OrmLiteConfig.DialectProvider.UseUnicode = true;
+            var createTableSql = OrmLiteConfig.DialectProvider.ToCreateTableStatement(typeof(ModelWithIdAndName));
+            Console.WriteLine("createTableSql: " + createTableSql);
+            Assert.That(createTableSql.Contains("NVARCHAR(4000)"), Is.True);
+        }
+
+        [Test]
+        public void Can_create_table_with_string_longer_than_4000_for_unicode()
+        {
+            using(var con = ConnectionString.OpenDbConnection())
+            {
+                OrmLiteConfig.DialectProvider.UseUnicode = true;
+
+                Assert.DoesNotThrow(
+                    () =>
+                    {
+                        con.CreateTable<ModelWithIdAndLongString>(true);
+                    }
+                );
+            }
+        }
+
+        [Test]
+        public void Can_store_unicode_string()
+        {
+            using(var con = ConnectionString.OpenDbConnection())
+            {
+                OrmLiteConfig.DialectProvider.UseUnicode = true;
+                ModelWithIdAndName model = new ModelWithIdAndName()
+                {
+                    Id = 1,
+                    Name = "日本語 äÄöÖüÜß ıIiİüÜğĞşŞöÖçÇ åÅäÄöÖ"
+                };
+
+                con.CreateTable<ModelWithIdAndName>(true);
+                con.Save(model);
+
+                var result = con.GetById<ModelWithIdAndName>(1);
+                con.DropTable<ModelWithIdAndName>();
+
+                Console.WriteLine("Inserted: {0}, Retrieved: {1}", model.Name, result.Name);
+                Assert.That(model.Name.Equals(result.Name));
+            }
+        }
+
+        [Test]
+        public void Can_store_string_longer_than_4000_characters()
+        {
+            using(var con = ConnectionString.OpenDbConnection())
+            {
+                string longString = "";
+
+                while(longString.Length < 9000)
+                {
+                    longString += "日本語 äÄöÖüÜß ıIiİüÜğĞşŞöÖçÇ åÅäÄöÖ";
+                }
+
+                longString = longString.Substring(0, 8500);
+
+                Assert.That(longString.Length == 8500);
+
+                ModelWithIdAndLongString model = new ModelWithIdAndLongString()
+                {
+                    Id = 1,
+                    LongString = longString
+                };
+
+                con.CreateTable<ModelWithIdAndLongString>(true);
+                con.Save(model);
+
+                var result = con.GetById<ModelWithIdAndLongString>(1);
+                con.DropTable<ModelWithIdAndLongString>();
+
+                Console.WriteLine("Inserted: {0}, Retrieved: {1}", model.LongString, result.LongString);
+                Assert.That(model.LongString.Equals(result.LongString));
+            }
+        }
+    }
+
+    [TestFixture]
+    public class NonUnicodeStringTests : OrmLiteTestBase
+    {
+        public class ModelWithIdAndLongString
+        {
+            public int Id { get; set; }
+
+            [StringLength(9000)]
+            public string LongString { get; set; }
+        }
+
+        class ComplexType
+        {
+            public int Id { get; set; }
+
+            [StringLength(250)]
+            public IList<string> StringList { get; set; }
+            [StringLength(8000)]
+            public Dictionary<Guid, string> Dictionary { get; set; }
+            public IList<ComplexSubType> SubTypes { get; set; }
+
+            internal class ComplexSubType
+            {
+                public int Field1 { get; set; }
+                public string Field2 { get; set; }
+                public Guid Field3 { get; set; }
+                public DateTime Field4 { get; set; }
+            }
+        }
+
+        [Test]
+        public void Can_create_table_with_string_longer_than_8000()
+        {
+            using(var con = ConnectionString.OpenDbConnection())
+            {
+                Assert.DoesNotThrow(
+                    () =>
+                    {
+                        con.CreateTable<ModelWithIdAndLongString>(true);
+                    }
+                );
+            }
+        }
+
+        [Test]
+        public void Can_store_string_longer_than_8000_characters()
+        {
+            using(var con = ConnectionString.OpenDbConnection())
+            {
+                string longString = "";
+
+                while(longString.Length < 9000)
+                {
+                    longString += "abcdefghijklmnopqrstuvwxyz";
+                }
+
+                longString = longString.Substring(0, 8500);
+
+                Assert.That(longString.Length == 8500);
+
+                ModelWithIdAndLongString model = new ModelWithIdAndLongString()
+                {
+                    Id = 1,
+                    LongString = longString
+                };
+
+                con.CreateTable<ModelWithIdAndLongString>(true);
+                con.Save(model);
+
+                var result = con.GetById<ModelWithIdAndLongString>(1);
+                con.DropTable<ModelWithIdAndLongString>();
+
+                Console.WriteLine("Inserted: {0}, Retrieved: {1}", model.LongString, result.LongString);
+                Assert.That(model.LongString.Equals(result.LongString));
+            }
+        }
+
+        [Test]
+        public void Complex_Types_Should_Use_Max_For_Undefined_String_Length()
+        {
+            var createTableSql = OrmLiteConfig.DialectProvider.ToCreateTableStatement(typeof(ComplexType));
+            Console.WriteLine("createTableSql: " + createTableSql);
+            Assert.That(createTableSql.Contains(@"""StringList"" VARCHAR(250)"), Is.True);
+            Assert.That(createTableSql.Contains(@"""Dictionary"" VARCHAR(8000)"), Is.True);
+            Assert.That(createTableSql.Contains(@"""SubTypes"" VARCHAR(MAX)"), Is.True);
+        }
+    }
+}
